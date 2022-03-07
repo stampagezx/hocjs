7a1b63848e7babbea12a7499a7145fa69ebc0ecf
stampagezx <93703811+stampagezx@users.noreply.github.com>
2022-03-07 09:42:48 +0700

    sửa logic

--- Content.js
+++ Content.js
@@ -1,3 +1,8 @@
-setInterval(function() {
-    window.scrollBy(0, 500)
-}, 1000);
+var myVar = setInterval(scrolltime, 3000);
+function scrolltime() {
+    window.scrollBy(0, 500);
+}
+function stopscroll() {
+    clearInterval(myVar);
+}
+setTimeout(stopscroll, 20000);
