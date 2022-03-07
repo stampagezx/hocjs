79a2aa0ab4f205d0db1bf33e28ab2181474f2f51
stampagezx <93703811+stampagezx@users.noreply.github.com>
2022-03-07 09:38:04 +0700

    giao diện

---
+++ main.js
@@ -0,0 +1,13 @@
+let startBtn = document.querySelector('.start-btn')
+let breakBtn = document.querySelector('.break-btn')
+let continueBtn = document.querySelector('.continue-btn')
+
+startBtn.addEventListener('click', () => {
+    console.log('start');
+})
+breakBtn.addEventListener('click', () => {
+    console.log('break');
+})
+continueBtn.addEventListener('click', () => {
+    console.log('continue');
+})
